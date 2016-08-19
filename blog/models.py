0a8b6bac38040b637f6a2a1de279ff1344a3c662
Isaiah <itorain12@gmail.com>
2016-08-19 02:16:14 -0600

    Added tests for posts

--- blog/models.py
+++ blog/models.py
@@ -4,34 +4,46 @@ from django.utils.text import slugify
 from django.core.urlresolvers import reverse
 from django.utils import timezone
 
+class PostManager(models.Manager):
+    def all(self):
+        return super(PostManager, self).filter(published=True) # will only show posts that are published
+
 class Post(models.Model):
 	title = models.CharField(max_length=100, unique=True)
 	author = models.ForeignKey('auth.user')
 	slug = models.SlugField(unique=True)
 	body = models.TextField()
 	description = models.TextField(max_length=200)
-	publish = models.BooleanField(default=False)
 	created = models.DateTimeField(auto_now_add=True, auto_now=False)
 	category = models.ForeignKey('blog.Category', blank=True, null=True)
 	updated = models.DateTimeField(db_index=True, blank=True, null=True)
+	published = models.BooleanField(default=False)
 	tags = models.ManyToManyField('blog.Tag')
+	objects = PostManager()
 	
 	def __unicode__(self):
-		return unicode(self.title)
-		
-	def publish(self):
-		self.publish = True
-		self.updated = timezone.now()
-		self.save()
+		return str(self.title)
 		
 	def get_absolute_url(self):
 		return reverse('blog.views.post', args=[self.slug])
+		
+	def publish(self):
+		self.published = True
+		self.updated = timezone.now()
+		self.save()
+		
+	# Adding a way to next and previous posts for end users	
+	def get_previous_post(self):
+		return self.get_previous_by_updated()
+		
+	def get_next_post(self):
+		return self.get_next_by_updated()
 		
 	class Meta:
 		ordering = ['-updated']	
 		
 class Category(models.Model):
-	name = models.CharField(max_length=50)
+	name = models.CharField(max_length=50, db_index=True)
 	description = models.TextField()
 	slug = models.SlugField(max_length=50, unique=True, blank=True, null=True)
 	
@@ -41,7 +53,7 @@ class Category(models.Model):
 		super(Category, self).save()
 	
 	def get_absolute_url(self):
-		return "/category/%s" % (self.slug)
+		return reverse('blog.views.category', args=[self.slug])
 	
 	def __unicode__(self):
 		return self.name
@@ -50,7 +62,7 @@ class Category(models.Model):
 		verbose_name_plural = 'categories'
 		
 class Tag(models.Model):
-	name = models.CharField(max_length=20)
+	name = models.CharField(max_length=20, db_index=True)
 	description = models.TextField(max_length=255, null=True, default='')
 	slug = models.SlugField(max_length=50, unique=True, blank=True, null=True)
 	
@@ -60,7 +72,7 @@ class Tag(models.Model):
 		super(Tag, self).save()
 		
 	def get_absolute_url(self):
-		return "/tag/%s/" % (self.slug)
+		return reverse('blog.views.tag', args=[self.slug])
 		
 	def __unicode__(self):
 		return self.name
--- blog/tests.py
+++ blog/tests.py
@@ -1,17 +1,25 @@
 from django.test import TestCase
 from django.utils import timezone
 from blog.models import Post
+from django.contrib.auth.models import User
 
 # Test cases for the post class
 class PostTest(TestCase):
 	def test_create_post(self):
 	# Create the post
 		post = Post()
+	# Create the author
+		user = User.objects.create_user('mert', 'mert@test.com', 'password')
+		user.save()
 	# Set the attributes	
 		post.title = 'Test Post'
 		post.body = 'This is a test post!'
+		post.description = 'a description'
+		post.slug = 'test-post'
+		post.author = user
 		post.created = timezone.now()
 		post.updated = timezone.now()
+		post.published = True # Need this or Post.objects.all() will filter and will not return this
 	# Save it
 		post.save()
 	# Check if I can find post
@@ -19,12 +27,16 @@ class PostTest(TestCase):
 		self.assertEquals(len(allPosts), 1)
 		myPost = allPosts[0]
 		self.assertEquals(myPost, post)
-	# Check attributes
+		# Check attributes
 		self.assertEquals(myPost.title, 'Test Post')
-        self.assertEquals(myPost.text, 'This is a test post')
-        self.assertEquals(myPost.created.day, post.created.day)
-        self.assertEquals(myPost.created.month, post.created.month)
-        self.assertEquals(myPost.created.year, post.created.year)
-        self.assertEquals(myPost.created.hour, post.created.hour)
-        self.assertEquals(myPost.created.minute, post.created.minute)
-        self.assertEquals(myPost.created.second, post.created.second)
+		self.assertEquals(myPost.body, 'This is a test post!')
+		self.assertEquals(myPost.description, 'a description')
+		self.assertEquals(myPost.slug, 'test-post')
+		self.assertEquals(myPost.author.username, 'mert')
+		self.assertEquals(myPost.author.email, 'mert@test.com')
+		self.assertEquals(myPost.created.day, post.created.day)
+		self.assertEquals(myPost.created.month, post.created.month)
+		self.assertEquals(myPost.created.year, post.created.year)
+		self.assertEquals(myPost.created.hour, post.created.hour)
+		self.assertEquals(myPost.created.minute, post.created.minute)
+		self.assertEquals(myPost.created.second, post.created.second)
